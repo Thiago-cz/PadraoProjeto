6fcf8aedda4dc302f2d9c32c35d03b11be09a87d
Thiago Carvalho de Souza <102882911+Thiago-cz@users.noreply.github.com>
2024-09-12 18:44:41 -0300

    Update Main.java

--- Atividade 12/Main.java
+++ Atividade 12/Main.java
@@ -1,3 +1,11 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        CaixaAssinatura caixaBronze = LojaQuadrinhos.criarCaixaAleatoria("Bronze");
+        caixaBronze.listarItens();
+
+        System.out.println();
+
+        CaixaAssinatura caixaPlatina = LojaQuadrinhos.criarCaixaAleatoria("Platina");
+        caixaPlatina.listarItens();
+    }
 }
